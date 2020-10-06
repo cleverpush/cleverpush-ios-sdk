5cf226233ba5d8bc40d279cd76c1240f6e4fce92
gbhrdt <marius.gebhardt@gmail.com>
2020-10-06 18:07:44 +0200

    Release 1.1.0

---
+++ CleverPush/CleverPush/Source/IntrinsicTableView.h
@@ -0,0 +1,13 @@
+//
+//  IntrinsicTableView.h
+//  CleverPush
+//
+//  Created by Marius Gebhardt on 06/10/2020.
+//  Copyright © 2020 CleverPush. All rights reserved.
+//
+
+#ifndef IntrinsicTableView_h
+#define IntrinsicTableView_h
+
+
+#endif /* IntrinsicTableView_h */
---
+++ CleverPush/CleverPush/Source/IntrinsicTableView.m
@@ -0,0 +1,9 @@
+//
+//  IntrinsicTableView.m
+//  CleverPush
+//
+//  Created by Marius Gebhardt on 06/10/2020.
+//  Copyright © 2020 CleverPush. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
