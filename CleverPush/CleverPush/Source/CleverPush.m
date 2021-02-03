2b8c6c49ecad257494e6943fff5327bc6ab6928f
gbhrdt <marius.gebhardt@gmail.com>
2021-02-03 22:04:50 +0100

    Build 1.4.1

--- CleverPush/CleverPush/Source/CleverPush.m
+++ CleverPush/CleverPush/Source/CleverPush.m
@@ -68,7 +68,7 @@
 
 @implementation CleverPush
 
-NSString * const CLEVERPUSH_SDK_VERSION = @"1.4.0";
+NSString * const CLEVERPUSH_SDK_VERSION = @"1.4.1";
 
 static BOOL registeredWithApple = NO;
 static BOOL startFromNotification = NO;
