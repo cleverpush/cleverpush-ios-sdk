a053eacf1f647f29c339d47756af027f13a49680
Marius <gbhrdt@users.noreply.github.com>
2021-10-29 21:58:49 +0200

    Update CPImageBlockCell.m

--- CleverPush/CleverPush/Source/CPImageBlockCell.m
+++ CleverPush/CleverPush/Source/CPImageBlockCell.m
@@ -1,23 +1,13 @@
-//
-//  CPImageBlockCell.m
-//  CleverPush
-//
-//  Created by Azhar - M1 on 26/10/21.
-//
-
 #import "CPImageBlockCell.h"
 
 @implementation CPImageBlockCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
