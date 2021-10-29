3e2decd5b62bfd544f87d0155f2807481e2c69aa
Marius <gbhrdt@users.noreply.github.com>
2021-10-29 21:59:07 +0200

    Update CPTextBlockCell.m

--- CleverPush/CleverPush/Source/CPTextBlockCell.m
+++ CleverPush/CleverPush/Source/CPTextBlockCell.m
@@ -1,23 +1,13 @@
-//
-//  CPTextBlockCell.m
-//  CleverPush
-//
-//  Created by Azhar - M1 on 26/10/21.
-//
-
 #import "CPTextBlockCell.h"
 
 @implementation CPTextBlockCell
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
 }
 
 @end
