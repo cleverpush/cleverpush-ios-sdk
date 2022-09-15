499bfb577e31934fc2f154a0c73cbcd3c4871084
cpandya25 <c.pandya@cleverpush.com>
2022-09-15 19:48:30 +0530

    feature/CP-3923-ios-set-up-example-project-inside-sdk
    Example project created into sdk.

---
+++ CleverPush/CleverPushExample/CleverPushExample/ViewController.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+- (IBAction)btnHandlerSubscriptionOn:(id)sender;
+- (IBAction)btnHandlerSubscribeOff:(id)sender;
+- (IBAction)btnHandlergetSubscriptionID:(id)sender;
+ 
+@property (weak, nonatomic) IBOutlet UILabel *lblStatusDisplay;
+
+@end
---
+++ CleverPush/CleverPushExample/CleverPushExample/ViewController.m
@@ -0,0 +1,36 @@
+#import "ViewController.h"
+#import <CleverPush/CleverPush.h>
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (IBAction)btnHandlergetSubscriptionID:(id)sender {
+    if ([CleverPush isSubscribed]) {
+        self.lblStatusDisplay.text = [NSString stringWithFormat:@"Subscribiton id %@",[CleverPush getSubscriptionId]];
+    } else {
+        self.lblStatusDisplay.text = @"Subscription Status is No";
+    }
+}
+
+- (IBAction)btnHandlerSubscribeOff:(id)sender {
+    [CleverPush unsubscribe:^(BOOL issubscribe) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.lblStatusDisplay.text = [NSString stringWithFormat:@"Subscribiton Status %@",[CleverPush isSubscribed] ? @"Yes" : @"No"];
+        });
+    }];
+}
+
+- (IBAction)btnHandlerSubscriptionOn:(id)sender {
+    [CleverPush subscribe:^(NSString *result) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.lblStatusDisplay.text = [NSString stringWithFormat:@"Subscribiton Status %@",[CleverPush isSubscribed] ? @"Yes" : @"No"];
+        });
+    }];
+}
+@end
