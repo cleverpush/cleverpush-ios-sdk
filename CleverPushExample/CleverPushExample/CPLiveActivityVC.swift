3e914f1d9062021c8d19eec156ea34d1e9b7e091
cpandya25 <c.pandya@cleverpush.com>
2022-12-05 08:56:32 +0530

    feature/CP-4747-ios-16-live-activities
    Live activity declaration changes.

--- CleverPushExample/CleverPushExample/CPLiveActivityVC.swift
+++ CleverPushExample/CleverPushExample/CPLiveActivityVC.swift
@@ -2,12 +2,15 @@ import Foundation
 import ActivityKit
 import UserNotifications
 
-public struct CleverPushWidgetExtensionAttributes: ActivityAttributes {
+struct CleverPushWidgetExtensionAttributes: ActivityAttributes, Identifiable {
+    public typealias LiveDeliveryData = ContentState
     public struct ContentState: Codable, Hashable {
         var message: String
     }
+    var id = UUID()
     var title: String
 }
+
 
 @objc
 class CPLiveActivityVC: NSObject {
@@ -15,15 +18,20 @@ class CPLiveActivityVC: NSObject {
     @available(iOS 13.0, *)
     @objc
     static func createActivity() async -> String? {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let error = error {
+            }
+        }
         if #available(iOS 16.1, *) {
             counter += 1;
-            let attributes = CleverPushWidgetExtensionAttributes(title: "CleverPush")
-            let contentState = CleverPushWidgetExtensionAttributes.ContentState(message: "Live Activity Started")
+            let attributes = CleverPushWidgetExtensionAttributes(title: "CleverPush ")
+            let contentState = CleverPushWidgetExtensionAttributes.LiveDeliveryData(message: "Live Activity Stared")
             do {
                 let activity = try Activity<CleverPushWidgetExtensionAttributes>.request(
-                        attributes: attributes,
-                        contentState: contentState,
-                        pushType: .token)
+                    attributes: attributes,
+                    contentState: contentState,
+                    pushType: .token)
                 for await data in activity.pushTokenUpdates {
                     let myToken = data.map {String(format: "%02x", $0)}.joined()
                     return myToken
