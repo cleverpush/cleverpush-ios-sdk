387e9ada496235c7cbe67e7f01f9d2b93350bc32
cpandya25 <c.pandya@cleverpush.com>
2023-10-06 18:07:31 +0530

    hotfix/CP-6786-app-banner-issues
    implemented set or get `UIViewController` as a custom top view controller.

--- CleverPush/Source/CleverPush.h
+++ CleverPush/Source/CleverPush.h
@@ -129,6 +129,7 @@ extern NSString * const CLEVERPUSH_SDK_VERSION;
 + (void)getAppBannersByGroup:(NSString*)groupId callback:(void(^)(NSMutableArray <CPAppBanner*>*))callback;
 + (void)setApiEndpoint:(NSString*)apiEndpoint;
 + (void)setAuthorizerToken:(NSString*)authorizerToken;
++ (void)setCustomTopViewController:(UIViewController*)viewController;
 + (void)updateBadge:(UNMutableNotificationContent*)replacementContent API_AVAILABLE(ios(10.0));
 + (void)addStoryView:(CPStoryView*)storyView;
 + (void)updateDeselectFlag:(BOOL)value;
@@ -153,6 +154,7 @@ extern NSString * const CLEVERPUSH_SDK_VERSION;
 + (NSString*)getSubscriptionId;
 + (NSString*)getApiEndpoint;
 + (NSString*)channelId;
++ (UIViewController*)getCustomTopViewController;
 
 + (UIColor*)getBrandingColor;
 
