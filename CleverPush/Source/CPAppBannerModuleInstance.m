a72900414c1c0755ca2245cbe97a19b7c994a98f
cpandya25 <c.pandya@cleverpush.com>
2024-03-28 15:24:24 +0530

    hotfix/CP-7571-ios-crashes-in-app-v1
    Optimized banner objects prevent crashes.

--- CleverPush/Source/CPAppBannerModuleInstance.m
+++ CleverPush/Source/CPAppBannerModuleInstance.m
@@ -635,7 +635,9 @@ NSInteger currentScreenIndex = 0;
 }
 
 - (BOOL)checkEventTriggerCondition:(CPAppBannerTriggerCondition*)condition {
-    for (NSDictionary* triggeredEvent in events) {
+    NSMutableArray<NSDictionary*> *eventsCopy = [events mutableCopy];
+
+    for (NSDictionary* triggeredEvent in eventsCopy) {
         if (![[triggeredEvent cleverPushStringForKey:@"id"] isEqualToString:condition.event]) {
             continue;
         }
