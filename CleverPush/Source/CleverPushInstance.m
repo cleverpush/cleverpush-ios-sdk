9a42e7dfe785f971a5685f62805e8ae0a9d475ab
cpandya25 <110810122+cpandya25@users.noreply.github.com>
2024-09-10 15:51:24 +0530

    hotfix/CP-8535-ios-18-beta-crash-in-track-session-end (#248)
    * hotfix/CP-8535-ios-18-beta-crash-in-track-session-end
    
    Optimised  `trackSessionEnd` to prevent crash.
    
    * hotfix/CP-8535-ios-18-beta-crash-in-track-session-end
    
    Optimised `trackSessionEnd` to prevent crash.
    
    * hotfix/CP-8535-ios-18-beta-crash-in-track-session-end
    
    Optimised `trackSessionEnd` to prevent crash.

--- CleverPush/Source/CleverPushInstance.m
+++ CleverPush/Source/CleverPushInstance.m
@@ -3507,14 +3507,19 @@ static id isNil(id object) {
 
                     long sessionEndedTimestamp = [[NSDate date] timeIntervalSince1970];
                     long sessionDuration = sessionEndedTimestamp - sessionStartedTimestamp;
+                    long visits = MAX(sessionVisits, 0);
+
+                    if (channelId == nil || subscriptionId == nil || deviceToken == nil || sessionDuration < 0) {
+                        return;
+                    }
 
                     NSMutableURLRequest* request = [[CleverPushHTTPClient sharedClient] requestWithMethod:HTTP_POST path:@"subscription/session/end"];
                     NSDictionary* dataDic = [NSDictionary dictionaryWithObjectsAndKeys:
                                              channelId, @"channelId",
                                              subscriptionId, @"subscriptionId",
                                              deviceToken, @"apnsToken",
-                                             sessionVisits, @"visits",
-                                             sessionDuration, @"duration",
+                                             @(visits), @"visits",
+                                             @(sessionDuration), @"duration",
                                              nil];
 
                     NSData* postData = [NSJSONSerialization dataWithJSONObject:dataDic options:0 error:nil];
