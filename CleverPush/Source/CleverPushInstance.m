0223e17216532ad9ded0d77ce14bddd39a42f6d2
Marius <gbhrdt@users.noreply.github.com>
2024-10-04 09:20:51 +0200

    CP-8677 Optimized getChannelConfig (#253)

--- CleverPush/Source/CleverPushInstance.m
+++ CleverPush/Source/CleverPushInstance.m
@@ -936,19 +936,28 @@ static id isNil(id object) {
 
 #pragma mark - API call and get the data of the specific Channel.
 - (void)getChannelConfig:(void(^ _Nullable)(NSDictionary* _Nullable))callback {
-    if (channelConfig) {
-        callback(channelConfig);
-        return;
+    @synchronized(self) {
+        if (channelConfig) {
+            if (callback) {
+                callback(channelConfig);
+            }
+            return;
+        }
+
+        if (!pendingChannelConfigListeners) {
+            pendingChannelConfigListeners = [NSMutableArray new];
+        }
+        
+        [pendingChannelConfigListeners addObject:callback];
+        if (pendingChannelConfigRequest) {
+            return;
+        }
+        pendingChannelConfigRequest = YES;
     }
 
-    [pendingChannelConfigListeners addObject:callback];
-    if (pendingChannelConfigRequest) {
-        return;
-    }
-    pendingChannelConfigRequest = YES;
-
-    NSString*configPath = @"";
-    if ([self channelId] != NULL) {
+    NSString *configPath = @"";
+    NSString *channelId = [self channelId];
+    if (channelId != NULL) {
         configPath = [NSString stringWithFormat:@"channel/%@/config?platformName=iOS", channelId];
         if ([self isDevelopmentModeEnabled]) {
             configPath = [NSString stringWithFormat:@"%@&t=%f", configPath, NSDate.date.timeIntervalSince1970];
@@ -959,6 +968,7 @@ static id isNil(id object) {
         [self getChannelConfigFromBundleId:configPath];
     }
 }
+
 - (NSString* _Nullable)getBundleName {
     return [[NSBundle mainBundle] bundleIdentifier];
 }
