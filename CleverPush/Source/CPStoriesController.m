802039aa5b63a1f515251c89b038e7e5be0db535
cpandya25 <c.pandya@cleverpush.com>
2024-11-15 12:52:38 +0530

    hotfix/CP-8804-ios-sdk-story-widget-customisations-for-loading-view-in-story-detail-screen
    Add story loading animation in widget (border_color_loading, border_color_loading_dark_mode)
    To remove white load html content, click on the widget once it is loaded, then display the detail screen.

--- CleverPush/Source/CPStoriesController.m
+++ CleverPush/Source/CPStoriesController.m
@@ -124,103 +124,106 @@
 
     self.contentLoadedCompletion = completion;
 
-    NSMutableString *anchorTags = [NSMutableString stringWithString:@"[\n"];
-    BOOL hideShareButton = !self.storyWidgetShareButtonVisibility;
-
+    NSMutableArray *storyURLs = [NSMutableArray array];
     for (NSInteger i = 0; i < self.stories.count; i++) {
         NSString *storyId = self.stories[i].id;
-        NSString *customURL = @"";
         NSInteger subStoryIndex = [self getSubStoryPosition:i];
-        NSString *hideShareButtonValue = @"false";
-        if (hideShareButton) {
-            hideShareButtonValue = @"true";
-        }
+        BOOL hasMultiplePages = (self.stories[i].content.pages != nil && self.stories[i].content.pages.count > 1);
 
-        if (self.stories[i].content.pages != nil && self.stories[i].content.pages.count > 1) {
-            customURL = [NSString stringWithFormat:@"https://api.cleverpush.com/channel/%@/story/%@/html?hideStoryShareButton=%@&widgetId=%@&#page=page-%ld",
-                         self.stories[i].channel, storyId, hideShareButtonValue, self.widget.id, (long)subStoryIndex];
-        } else {
-            customURL = [NSString stringWithFormat:@"https://api.cleverpush.com/channel/%@/story/%@/html?hideStoryShareButton=%@&widgetId=%@",
-                         self.stories[i].channel, storyId, hideShareButtonValue, self.widget.id];
-        }
+        NSString *customURL = [NSString stringWithFormat:@"https://api.cleverpush.com/channel/%@/story/%@/html?hideStoryShareButton=%@&widgetId=%@%@",
+                               self.stories[i].channel,
+                               storyId,
+                               self.storyWidgetShareButtonVisibility ? @"false" : @"true",
+                               self.widget.id,
+                               hasMultiplePages ? [NSString stringWithFormat:@"&#page=page-%ld", (long)subStoryIndex] : @""];
 
-        [anchorTags appendFormat:@"    {\n"];
-        [anchorTags appendFormat:@"        href: '%@'\n", customURL];
-        [anchorTags appendFormat:@"    }"];
-
-        if (i < self.stories.count - 1) {
-            [anchorTags appendString:@",\n"];
-        } else {
-            [anchorTags appendString:@"\n"];
-        }
+        [storyURLs addObject:customURL];
     }
 
-    [anchorTags appendString:@"]"];
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:storyURLs options:0 error:&error];
+    NSString *storyURLsJsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
 
-    NSString *html = [NSString stringWithFormat:@"\
-                                <!DOCTYPE html>\
-                                <html>\
-                                <head>\
+    NSString *html = [NSString stringWithFormat:
+                              @"<!DOCTYPE html>\
+                              <html>\
+                              <head>\
+                                <style>\
+                                  body { margin: 0; padding: 0; }\
+                                  amp-story-player { display: block; margin: 0; padding: 0; width: 100%%; height: %@; }\
+                                </style>\
                                 <script src=\"https://cdn.ampproject.org/amp-story-player-v0.js\"></script>\
                                 <link rel=\"stylesheet\" href=\"https://cdn.ampproject.org/amp-story-player-v0.css\">\
-                                <style>\
-                                body {\
-                                        margin: 0;\
-                                        padding: 0;\
-                                }\
-                                amp-story-player {\
-                                        display: none;\
-                                        margin: 0;\
-                                        padding: 0;\
-                                        width: 100%%;\
-                                        height: %@;\
-                                }\
-                                </style>\
-                                </head>\
-                                <body>\
-                                <amp-story-player>\
-                                </amp-story-player>\
+                              </head>\
+                              <body>\
                                 <script>\
-                                var playerEl = document.querySelector('amp-story-player');\
-                                var player = new AmpStoryPlayer(window, playerEl);\
-                                window.player = player;\
-                                playerEl.addEventListener('noPreviousStory', function (event) {\
-								    window.webkit.messageHandlers.previous.postMessage(%@);\
-								});\
-                                playerEl.addEventListener('noNextStory', function (event) {\
-								    window.webkit.messageHandlers.next.postMessage(%@);\
-								});\
-                                playerEl.addEventListener('storyNavigation', function (event) {\
-                                    var subStoryIndex = Number(event.detail.pageId?.split('-')?.[1] || 0);\
-                                    window.webkit.messageHandlers.storyNavigation.postMessage({ selectedPosition: %@, subStoryIndex: subStoryIndex });\
-                                });\
-                                playerEl.addEventListener('ready', function (event) {\
-                                    player.add(%@);\
+                                  function loadAmpResources(callback) {\
+                                    if (window.ampStoryPlayerLoaded) {\
+                                      callback();\
+                                      return;\
+                                    }\
+                                    window.ampStoryPlayerLoaded = true;\
+                                    const script = document.createElement('script');\
+                                    const link = document.createElement('link');\
+                                    script.src = 'https://cdn.ampproject.org/amp-story-player-v0.js';\
+                                    script.async = true;\
+                                    script.onload = function() {\
+                                      callback();\
+                                    };\
+                                    link.href = 'https://cdn.ampproject.org/amp-story-player-v0.css';\
+                                    link.rel = 'stylesheet';\
+                                    document.head.append(script, link);\
+                                  }\
+                                  function onPlayerReady(player) {\
                                     player.go(%@);\
-                                    playerEl.style.display = 'block';\
                                     window.webkit.messageHandlers.storyReady.postMessage({});\
-                                });\
-                                playerEl.addEventListener('navigation', function (event) {\
-                                    window.webkit.messageHandlers.navigation.postMessage({ index: event.detail.index });\
-                                });\
-                                window.addEventListener('message', function (event) {\
-                                    try {\
+                                  }\
+                                  loadAmpResources(function() {\
+                                    var playerEl = document.createElement('amp-story-player');\
+                                    var storyURLs = %@;\
+                                    storyURLs.forEach(function(storyURL) {\
+                                      var anker = document.createElement('a');\
+                                      anker.setAttribute('href', storyURL);\
+                                      playerEl.appendChild(anker);\
+                                    });\
+                                    var player = new AmpStoryPlayer(window, playerEl);\
+                                    document.body.appendChild(playerEl);\
+                                    player.load();\
+                                    window.player = player;\
+                                    if(player.isReady) {\
+                                      onPlayerReady(player);\
+                                    } else {\
+                                      player.addEventListener('ready', function(event) {\
+                                        onPlayerReady(player);\
+                                      });\
+                                    }\
+                                    playerEl.addEventListener('noNextStory', function(event) {\
+                                      window.webkit.messageHandlers.next.postMessage({});\
+                                    });\
+                                    playerEl.addEventListener('storyNavigation', function(event) {\
+                                      var subStoryIndex = Number(event.detail.pageId?.split('-')?.[1] || 0);\
+                                      window.webkit.messageHandlers.storyNavigation.postMessage({ selectedPosition: %@, subStoryIndex: subStoryIndex });\
+                                    });\
+                                    playerEl.addEventListener('navigation', function(event) {\
+                                      window.webkit.messageHandlers.navigation.postMessage({ index: event.detail.index });\
+                                    });\
+                                    window.addEventListener('message', function (event) {\
+                                      try {\
                                         var data = JSON.parse(event.data);\
                                         if (data.type === 'storyButtonCallback') {\
-                                            window.webkit.messageHandlers.storyButtonCallbackUrl.postMessage(data);\
+                                          window.webkit.messageHandlers.storyButtonCallbackUrl.postMessage(data);\
                                         }\
-                                    } catch (ignored) {}\
-								});\
+                                      } catch (ignored) {}\
+                                    });\
+                                  });\
                                 </script>\
-                                </body>\
-                                </html>",
-					  [NSString stringWithFormat:@"%fpx", [CPUtils frameHeightWithoutSafeArea]],
+                              </body>\
+                              </html>",
+                      [NSString stringWithFormat:@"%fpx", [CPUtils frameHeightWithoutSafeArea]],
                       @(self.storyIndex),
-					  @(self.storyIndex),
-					  @(self.storyIndex),
-                      anchorTags,
-					  @(self.storyIndex)
-	];
+                      storyURLsJsonString,
+                      @(self.storyIndex)
+    ];
 
     if (@available(iOS 16.4, *)) {
         [self.webview setInspectable:YES];
