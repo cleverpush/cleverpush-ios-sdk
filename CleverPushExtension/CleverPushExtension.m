db2ba7fdf607ad9c69edcc24c5bef31e08f99b81
cpandya25 <c.pandya@cleverpush.com>
2024-12-18 12:02:16 +0530

    hotfix/CP-8643-ios-18-crashes-separate-clever-push-extension-pod
    Optimised notification service extension service methods by creating separate sub-pods to prevent crashes.

---
+++ CleverPushExtension/CleverPushExtension.m
@@ -0,0 +1,19 @@
+#import <CleverPush/CleverPush.h>
+#import "CleverPushExtension.h"
+
+@interface CleverPushExtension ()
+
+@end
+
+@implementation CleverPushExtension
+
+
++ (UNMutableNotificationContent* _Nullable)didReceiveNotificationExtensionRequest:(UNNotificationRequest* _Nullable)request withMutableNotificationContent:(UNMutableNotificationContent* _Nullable)replacementContent API_AVAILABLE(ios(10.0)) {
+    return [CleverPush didReceiveNotificationExtensionRequest:request withMutableNotificationContent:replacementContent];
+}
+
++ (UNMutableNotificationContent* _Nullable)serviceExtensionTimeWillExpireRequest:(UNNotificationRequest* _Nullable)request withMutableNotificationContent:(UNMutableNotificationContent* _Nullable)replacementContent API_AVAILABLE(ios(10.0)) {
+    return [CleverPush serviceExtensionTimeWillExpireRequest:request withMutableNotificationContent:replacementContent];
+}
+
+@end
