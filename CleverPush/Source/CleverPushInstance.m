074f76c0ba49c851f77aec64d92de6a7b5bb2af5
cpandya25 <c.pandya@cleverpush.com>
2025-02-04 18:59:14 +0530

    Release 1.32.6
    Release 1.32.6

--- CleverPush/Source/CleverPushInstance.m
+++ CleverPush/Source/CleverPushInstance.m
@@ -73,7 +73,7 @@
 
 @implementation CleverPushInstance
 
-NSString* const CLEVERPUSH_SDK_VERSION = @"1.32.5";
+NSString* const CLEVERPUSH_SDK_VERSION = @"1.32.6";
 
 static BOOL registeredWithApple = NO;
 static BOOL startFromNotification = NO;
