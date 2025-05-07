67a78e81d747bc3540fbf130c770ac1a407a5894
gbhrdt <marius.gebhardt@gmail.com>
2025-05-07 12:21:10 +0200

    Release 1.34.9

--- CleverPush/Source/CleverPushInstance.m
+++ CleverPush/Source/CleverPushInstance.m
@@ -73,7 +73,7 @@
 
 @implementation CleverPushInstance
 
-NSString* const CLEVERPUSH_SDK_VERSION = @"1.34.8";
+NSString* const CLEVERPUSH_SDK_VERSION = @"1.34.9";
 
 static BOOL startFromNotification = NO;
 static BOOL autoClearBadge = YES;
