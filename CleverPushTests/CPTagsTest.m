f644843762ebeed7a0a405fd0aa2d7b912bc4267
cpandya25 <c.pandya.ext@cleverpush.com>
2026-08-25 16:19:44 +0530

    feature/cp-4348-ios-unit-testing
    Update CPTagsTest.m for tags related methods.

--- CleverPushTests/CPTagsTest.m
+++ CleverPushTests/CPTagsTest.m
@@ -289,20 +289,401 @@
     [self waitForExpectationsWithTimeout:1.0 handler:nil];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+#pragma mark - hasSubscriptionTag
+
+- (void)testHasSubscriptionTagReturnsTrueWhenTagExists {
+    [[NSUserDefaults standardUserDefaults] setObject:@[ @"existingTag" ] forKey:CLEVERPUSH_SUBSCRIPTION_TAGS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    XCTAssertTrue([self.testableInstance hasSubscriptionTag:@"existingTag"]);
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testHasSubscriptionTagReturnsFalseWhenTagDoesNotExist {
+    [[NSUserDefaults standardUserDefaults] setObject:@[ @"otherTag" ] forKey:CLEVERPUSH_SUBSCRIPTION_TAGS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    XCTAssertFalse([self.testableInstance hasSubscriptionTag:@"nonExistentTag"]);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
+- (void)testHasSubscriptionTagReturnsFalseWhenTagsListIsEmpty {
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:CLEVERPUSH_SUBSCRIPTION_TAGS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    XCTAssertFalse([self.testableInstance hasSubscriptionTag:@"anyTag"]);
+}
+
+- (void)testHasSubscriptionTagReturnsFalseForNilTagId {
+    [[NSUserDefaults standardUserDefaults] setObject:@[ @"existingTag" ] forKey:CLEVERPUSH_SUBSCRIPTION_TAGS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    XCTAssertFalse([self.testableInstance hasSubscriptionTag:nil]);
+}
+
+#pragma mark - getSubscriptionTags
+
+- (void)testGetSubscriptionTagsReturnsStoredTags {
+    NSArray *stored = @[ @"tag1", @"tag2" ];
+    [[NSUserDefaults standardUserDefaults] setObject:stored forKey:CLEVERPUSH_SUBSCRIPTION_TAGS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    NSArray *result = [self.testableInstance getSubscriptionTags];
+    XCTAssertEqualObjects(result, stored);
+}
+
+- (void)testGetSubscriptionTagsReturnsEmptyArrayWhenNothingStored {
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:CLEVERPUSH_SUBSCRIPTION_TAGS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    NSArray *result = [self.testableInstance getSubscriptionTags];
+    XCTAssertNotNil(result);
+    XCTAssertEqual(result.count, 0);
+}
+
+#pragma mark - getAvailableTags (callback variant)
+
+- (void)testGetAvailableTagsCallbackWithEmptyConfigReturnsEmptyArray {
+    [OCMStub([self.cleverPush getChannelConfig:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(NSDictionary *);
+        [invocation getArgument:&handler atIndex:2];
+        handler(nil);
     }];
+
+    [self.cleverPush getAvailableTags:^(NSArray *tags) {
+        XCTAssertNotNil(tags);
+        XCTAssertEqual(tags.count, 0);
+    }];
+}
+
+- (void)testGetAvailableTagsCallbackWithConfigHavingNoTagsReturnsEmptyArray {
+    NSDictionary *config = @{ @"channelTags": @[] };
+    [OCMStub([self.cleverPush getChannelConfig:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(NSDictionary *);
+        [invocation getArgument:&handler atIndex:2];
+        handler(config);
+    }];
+
+    [self.cleverPush getAvailableTags:^(NSArray *tags) {
+        XCTAssertNotNil(tags);
+        XCTAssertEqual(tags.count, 0);
+    }];
+}
+
+- (void)testGetAvailableTagsCallbackWithValidTagsReturnsTags {
+    NSMutableArray *tags = [NSMutableArray arrayWithObject:@"tag1"];
+    [OCMStub([self.cleverPush getAvailableTags:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(NSArray *);
+        [invocation getArgument:&handler atIndex:2];
+        handler(tags);
+    }];
+
+    [self.cleverPush getAvailableTags:^(NSArray *result) {
+        XCTAssertEqual(result.count, 1);
+        XCTAssertEqualObjects(result.firstObject, @"tag1");
+    }];
+}
+
+#pragma mark - addSubscriptionTag (no callback)
+
+- (void)testAddSubscriptionTagNoCallbackCallsWaitForTrackingConsent {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+
+    [self.cleverPush addSubscriptionTag:@"tagId"];
+    OCMVerify([self.cleverPush waitForTrackingConsent:[OCMArg any]]);
+}
+
+#pragma mark - addSubscriptionTag (callback only, no onFailure)
+
+- (void)testAddSubscriptionTagWithCallbackOnlyInvokesCallback {
+    XCTestExpectation *exp = [self expectationWithDescription:@"add tag callback only"];
+
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+
+    [OCMStub([self.cleverPush addSubscriptionTagToApi:[OCMArg any] callback:[OCMArg any] onFailure:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^callback)(NSString * _Nullable) = nil;
+        [invocation getArgument:&callback atIndex:3];
+        if (callback) callback(@"tagId");
+    }];
+
+    [self.cleverPush addSubscriptionTag:@"tagId" callback:^(NSString * _Nullable result) {
+        XCTAssertEqualObjects(result, @"tagId");
+        [exp fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:1.0 handler:nil];
+}
+
+#pragma mark - removeSubscriptionTag (no callback)
+
+- (void)testRemoveSubscriptionTagNoCallbackCallsWaitForTrackingConsent {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+
+    [self.cleverPush removeSubscriptionTag:@"tagId"];
+    OCMVerify([self.cleverPush waitForTrackingConsent:[OCMArg any]]);
+}
+
+#pragma mark - removeSubscriptionTag (callback only, no onFailure)
+
+- (void)testRemoveSubscriptionTagWithCallbackOnlyInvokesCallback {
+    XCTestExpectation *exp = [self expectationWithDescription:@"remove tag callback only"];
+
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+
+    [OCMStub([self.cleverPush removeSubscriptionTagFromApi:[OCMArg any] callback:[OCMArg any] onFailure:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^callback)(NSString * _Nullable) = nil;
+        [invocation getArgument:&callback atIndex:3];
+        if (callback) callback(@"tagId");
+    }];
+
+    [self.cleverPush removeSubscriptionTag:@"tagId" callback:^(NSString * _Nullable result) {
+        XCTAssertEqualObjects(result, @"tagId");
+        [exp fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:1.0 handler:nil];
+}
+
+#pragma mark - addSubscriptionTags (no callback)
+
+- (void)testAddSubscriptionTagsNoCallbackCallsAddForEachTag {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+    [OCMStub([self.cleverPush addSubscriptionTag:[OCMArg any] callback:[OCMArg any]]) andDo:^(NSInvocation *invocation) {}];
+
+    [self.cleverPush addSubscriptionTags:@[ @"t1", @"t2" ]];
+
+    OCMVerify([self.cleverPush addSubscriptionTag:@"t1" callback:[OCMArg any]]);
+    OCMVerify([self.cleverPush addSubscriptionTag:@"t2" callback:[OCMArg any]]);
+}
+
+#pragma mark - removeSubscriptionTags (no callback)
+
+- (void)testRemoveSubscriptionTagsNoCallbackCallsRemoveForEachTag {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+    [OCMStub([self.cleverPush removeSubscriptionTag:[OCMArg any] callback:[OCMArg any]]) andDo:^(NSInvocation *invocation) {}];
+
+    [self.cleverPush removeSubscriptionTags:@[ @"t1", @"t2" ]];
+
+    OCMVerify([self.cleverPush removeSubscriptionTag:@"t1" callback:[OCMArg any]]);
+    OCMVerify([self.cleverPush removeSubscriptionTag:@"t2" callback:[OCMArg any]]);
+}
+
+#pragma mark - addSubscriptionTags bulk - edge cases
+
+- (void)testAddSubscriptionTagsBulkWithEmptyArrayCallsCallbackWithCurrentTags {
+    XCTestExpectation *exp = [self expectationWithDescription:@"bulk add empty"];
+
+    NSArray *expected = @[ @"existing" ];
+    OCMStub([self.cleverPush getSubscriptionTags]).andReturn(expected);
+
+    [self.cleverPush addSubscriptionTags:@[] callback:^(NSArray<NSString *> * _Nullable result) {
+        XCTAssertEqualObjects(result, expected);
+        [exp fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:1.0 handler:nil];
+}
+
+- (void)testRemoveSubscriptionTagsBulkWithEmptyArrayCallsCallbackWithCurrentTags {
+    XCTestExpectation *exp = [self expectationWithDescription:@"bulk remove empty"];
+
+    NSArray *expected = @[ @"remaining" ];
+    OCMStub([self.cleverPush getSubscriptionTags]).andReturn(expected);
+
+    [self.cleverPush removeSubscriptionTags:@[] callback:^(NSArray<NSString *> * _Nullable result) {
+        XCTAssertEqualObjects(result, expected);
+        [exp fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:1.0 handler:nil];
+}
+
+#pragma mark - removeSubscriptionTag - not present in stored tags still calls API
+
+- (void)testRemoveSubscriptionTagNotInStoredTagsStillCallsApi {
+    [[NSUserDefaults standardUserDefaults] setObject:@[] forKey:CLEVERPUSH_SUBSCRIPTION_TAGS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+
+    [self.cleverPush removeSubscriptionTag:@"nonExistentTagId"];
+    OCMVerify([self.cleverPush removeSubscriptionTagFromApi:[OCMArg any] callback:[OCMArg any] onFailure:[OCMArg any]]);
+}
+
+#pragma mark - addSubscriptionTag with nil tagId
+
+- (void)testAddSubscriptionTagWithNilTagIdCallsApiWithNil {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+
+    [self.cleverPush addSubscriptionTag:nil];
+    OCMVerify([self.cleverPush waitForTrackingConsent:[OCMArg any]]);
+}
+
+#pragma mark - Tracking consent blocked
+
+- (void)testAddSubscriptionTagDoesNotCallApiWhenTrackingConsentNotGranted {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(true);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(false);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        // do not call the handler — consent not granted
+    }];
+
+    [[self.cleverPush reject] addSubscriptionTagToApi:[OCMArg any] callback:[OCMArg any] onFailure:[OCMArg any]];
+
+    [self.cleverPush addSubscriptionTag:@"tagId" callback:nil onFailure:nil];
+
+    OCMVerifyAll(self.cleverPush);
+}
+
+- (void)testRemoveSubscriptionTagDoesNotCallApiWhenTrackingConsentNotGranted {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(true);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(false);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        // do not call the handler — consent not granted
+    }];
+
+    [[self.cleverPush reject] removeSubscriptionTagFromApi:[OCMArg any] callback:[OCMArg any] onFailure:[OCMArg any]];
+
+    [self.cleverPush removeSubscriptionTag:@"tagId" callback:nil onFailure:nil];
+
+    OCMVerifyAll(self.cleverPush);
+}
+
+#pragma mark - addSubscriptionTagSuccessCallsCallback - nil callback safety
+
+- (void)testAddSubscriptionTagSuccessWithNilCallbackDoesNotCrash {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+
+    [OCMStub([self.cleverPush addSubscriptionTagToApi:[OCMArg any] callback:[OCMArg any] onFailure:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^callback)(NSString * _Nullable) = nil;
+        [invocation getArgument:&callback atIndex:3];
+        if (callback) callback(@"tagId");
+    }];
+
+    XCTAssertNoThrow([self.cleverPush addSubscriptionTag:@"tagId" callback:nil onFailure:nil]);
+}
+
+- (void)testRemoveSubscriptionTagSuccessWithNilCallbackDoesNotCrash {
+    OCMStub([self.cleverPush getTrackingConsentRequired]).andReturn(false);
+    OCMStub([self.cleverPush getHasTrackingConsent]).andReturn(true);
+    [OCMStub([self.cleverPush waitForTrackingConsent:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^handler)(void);
+        [invocation getArgument:&handler atIndex:2];
+        if (handler) handler();
+    }];
+
+    [OCMStub([self.cleverPush removeSubscriptionTagFromApi:[OCMArg any] callback:[OCMArg any] onFailure:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        void (^callback)(NSString * _Nullable) = nil;
+        [invocation getArgument:&callback atIndex:3];
+        if (callback) callback(@"tagId");
+    }];
+
+    XCTAssertNoThrow([self.cleverPush removeSubscriptionTag:@"tagId" callback:nil onFailure:nil]);
+}
+
+#pragma mark - addSubscriptionTagsBulk - multiple tags all succeed
+
+- (void)testAddSubscriptionTagsBulkAllTagsSucceedCallsCallbackWithFinalTags {
+    XCTestExpectation *exp = [self expectationWithDescription:@"bulk add all succeed"];
+
+    NSArray *expected = @[ @"a", @"b", @"c" ];
+    OCMStub([self.cleverPush getSubscriptionTags]).andReturn(expected);
+
+    [OCMStub([self.cleverPush addSubscriptionTag:[OCMArg any] callback:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        NSString *tagId = nil;
+        void (^callback)(NSString * _Nullable) = nil;
+        [invocation getArgument:&tagId atIndex:2];
+        [invocation getArgument:&callback atIndex:3];
+        if (callback) callback(tagId);
+    }];
+
+    [self.cleverPush addSubscriptionTags:@[ @"a", @"b", @"c" ] callback:^(NSArray<NSString *> * _Nullable result) {
+        XCTAssertEqualObjects(result, expected);
+        [exp fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:1.0 handler:nil];
+}
+
+#pragma mark - removeSubscriptionTagsBulk - multiple tags all succeed
+
+- (void)testRemoveSubscriptionTagsBulkAllTagsSucceedCallsCallbackWithFinalTags {
+    XCTestExpectation *exp = [self expectationWithDescription:@"bulk remove all succeed"];
+
+    NSArray *expected = @[];
+    OCMStub([self.cleverPush getSubscriptionTags]).andReturn(expected);
+
+    [OCMStub([self.cleverPush removeSubscriptionTag:[OCMArg any] callback:[OCMArg any]]) andDo:^(NSInvocation *invocation) {
+        NSString *tagId = nil;
+        void (^callback)(NSString * _Nullable) = nil;
+        [invocation getArgument:&tagId atIndex:2];
+        [invocation getArgument:&callback atIndex:3];
+        if (callback) callback(tagId);
+    }];
+
+    [self.cleverPush removeSubscriptionTags:@[ @"a", @"b" ] callback:^(NSArray<NSString *> * _Nullable result) {
+        XCTAssertEqualObjects(result, expected);
+        [exp fulfill];
+    }];
+
+    [self waitForExpectationsWithTimeout:1.0 handler:nil];
+}
+
+- (void)tearDown {
+    [[NSUserDefaults standardUserDefaults] removeObjectForKey:CLEVERPUSH_SUBSCRIPTION_TAGS_KEY];
+    [[NSUserDefaults standardUserDefaults] synchronize];
 }
 
 @end
