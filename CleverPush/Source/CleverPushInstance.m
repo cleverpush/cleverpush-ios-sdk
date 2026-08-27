68ecd5a56da172b50ef61cd50cbd30359b30865c
cpandya25 <c.pandya.ext@cleverpush.com>
2026-08-28 00:55:41 +0530

    Release 1.34.53
    Release 1.34.53

--- CleverPush/Source/CleverPushInstance.m
+++ CleverPush/Source/CleverPushInstance.m
@@ -74,7 +74,7 @@
 
 @implementation CleverPushInstance
 
-NSString* const CLEVERPUSH_SDK_VERSION = @"1.34.52";
+NSString* const CLEVERPUSH_SDK_VERSION = @"1.34.53";
 
 static BOOL startFromNotification = NO;
 static BOOL autoClearBadge = YES;
